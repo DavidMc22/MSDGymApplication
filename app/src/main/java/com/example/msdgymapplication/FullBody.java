7b90b371f3a519c82322d60814fe4230e5461ae4
Dave McCormack <leemccormack@Davids-MacBook-Pro.local>
2023-12-04 23:21:55 +0000

    FullBodyWorkout Page

--- app/src/main/java/com/example/msdgymapplication/FullBody.java
+++ app/src/main/java/com/example/msdgymapplication/FullBody.java
@@ -3,12 +3,79 @@ package com.example.msdgymapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class FullBody extends AppCompatActivity {
 
+    ListView listView;
+    ListView lv2;
+    ListView lv3;
+    ListView lv4;
+
+    //Data to be used to display in list
+    String[] FullBodyWorkout = {
+            "Bench Press\t\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\t\t\t3\t\t\t\t\t\t|\t\t\t\t8-10",
+            "Incline DB press\t\t\t\t\t\t\t|\t\t\t\t\t\t\t3\t\t\t\t\t\t|\t\t\t\t8-10"
+
+    };
+    String[] FBArms = {
+            "Bicep Curls\t\t\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\t\t\t3\t\t\t\t\t\t|\t\t\t\t8-10",
+            "Hammer curls\t\t\t\t\t\t\t\t\t|\t\t\t\t\t\t\t3\t\t\t\t\t\t|\t\t\t\t8-10",
+            "Tricep extension\t\t\t\t\t\t\t|\t\t\t\t\t\t\t3\t\t\t\t\t\t|\t\t\t\t8-10"
+    };
+
+    String[] FBBackShoulder = {
+            "Lat pull down\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\t\t\t3\t\t\t\t\t\t|\t\t\t\t8-10",
+            "Shoulder Press\t\t\t\t\t\t\t\t|\t\t\t\t\t\t\t3\t\t\t\t\t\t|\t\t\t\t8-10"
+    };
+
+    String[] FBLegs = {
+            "Hack squat\t\t\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\t\t\t3\t\t\t\t\t\t|\t\t\t\t8-10",
+            "Hamstring Curls\t\t\t\t\t\t\t|\t\t\t\t\t\t\t3\t\t\t\t\t\t|\t\t\t\t8-10"
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_body);
+
+        listView= findViewById(R.id.listview1);
+        lv2= findViewById(R.id.listView2);
+        lv3= findViewById(R.id.listview3);
+        lv4= findViewById(R.id.listview4);
+
+        ArrayAdapter Adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                FullBodyWorkout
+        );
+
+        listView.setAdapter(Adapter);
+
+        ArrayAdapter Adapter2 = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                FBArms
+        );
+
+        lv2.setAdapter(Adapter2);
+
+        ArrayAdapter Adapter3 = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                FBBackShoulder
+        );
+
+        lv3.setAdapter(Adapter3);
+
+        ArrayAdapter Adapter4 = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                FBLegs
+        );
+
+        lv4.setAdapter(Adapter4);
+
     }
 }
